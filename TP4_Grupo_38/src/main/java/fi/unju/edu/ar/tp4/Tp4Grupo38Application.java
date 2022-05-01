5425e460e855c2810daa7233657ca9863c3b29e6
GTL <GTL@DESKTOP-0LQB8T5>
2022-04-30 23:29:06 -0300

    Inicio

---
+++ TP4_Grupo_38/src/main/java/fi/unju/edu/ar/tp4/Tp4Grupo38Application.java
@@ -0,0 +1,13 @@
+package fi.unju.edu.ar.tp4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4Grupo38Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4Grupo38Application.class, args);
+	}
+
+}
